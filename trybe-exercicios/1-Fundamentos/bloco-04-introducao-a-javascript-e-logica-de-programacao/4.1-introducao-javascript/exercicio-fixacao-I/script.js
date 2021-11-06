d1ba9a160fc3bca075208c66ca70b6624b2ce7ef
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2021-11-06 00:15:36 -0300

    Atualiza README

---
+++ trybe-exercicios/1-Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.1-introducao-javascript/exercicio-fixacao-I/script.js
@@ -0,0 +1,10 @@
+const myName = "Fellipe";
+const birthCity = "Belo Horizonte";
+let birthYear = 1990;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = "São Paulo";
+console.log(birthCity); // não podemos reatribuir const //
