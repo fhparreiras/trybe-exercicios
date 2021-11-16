6ba01bf6c009d4be6cdd74d0437bdbfbb056b81b
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2021-11-16 20:37:45 -0300

    Exercícios bloco 5.1

---
+++ trybe-exercicios/1-Fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/5.1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll("h3")[0].style.backgroundColor = "pink";
+document.querySelectorAll("h3")[1].style.backgroundColor = "pink";
+document.querySelectorAll("h3")[2].style.backgroundColor = "black";
+document.querySelectorAll("h3")[3].style.backgroundColor = "black";
+document.querySelector(".emergency-tasks").style.backgroundColor = "#c5acd2";
+document.querySelector(".no-emergency-tasks").style.backgroundColor = "yellow";
+document.querySelector("footer div").style.backgroundColor = "green";
+document.querySelector("#header-container").style.backgroundColor = "green";
