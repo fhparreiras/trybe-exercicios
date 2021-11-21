e4fd59e35bea87b5487cd06edd6b90f6205e730c
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2021-11-21 00:40:19 -0300

    Exercícios bõnus 1 a 3 bloco 4.4

---
+++ trybe-exercicios/1-Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/4.4-javascript-objetos-e-funcoes/bonus/exercicio-bonus-3/script.js
@@ -0,0 +1,75 @@
+// Exercício bônus 3
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+  ];
+
+  function contaFrutas(basket){
+    let arrayLista = [];
+    let objetoLista = {};
+
+    for (let i = 0; i < basket.length; i += 1) {
+        if (arrayLista.includes(basket[i])) {
+            continue;
+        } else {
+            arrayLista.push(basket[i]);
+        }
+    }
+   
+    for (let c = 0; c < arrayLista.length; c += 1) {
+        objetoLista[arrayLista[c]] = 0;
+        for (let key of basket) {
+            if (key === arrayLista[c]) {
+                objetoLista[arrayLista[c]] += 1;
+            }
+        }
+    }
+
+    let resultado = '';
+    for (let f = 0; f < arrayLista.length; f += 1) {
+        if (f !== arrayLista.length - 1) {
+            resultado += objetoLista[arrayLista[f]] + ' ' + arrayLista[f] + 's, ';
+        } else {
+            resultado += objetoLista[arrayLista[f]] + ' ' + arrayLista[f] + 's.';  
+        }
+    }
+    console.log(resultado);
+    return objetoLista;
+ } 
+
+ contaFrutas([
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+  ]);
+
+  // 16 Melancias, 15 Abacates, 15 Uvas, 15 Laranjas, 15 Jacas, 11 Peras, 13 Bananas.
