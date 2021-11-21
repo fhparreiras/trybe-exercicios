e28d6f411c221683154caede3a45bf6cb1ed0dfc
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2021-11-21 08:35:22 -0300

    Bloco 5.3 exercício 2 ok

--- trybe-exercicios/1-Fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/5.3-javascript-eventos/exercicios-praticos/script.js
+++ trybe-exercicios/1-Fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/5.3-javascript-eventos/exercicios-praticos/script.js
@@ -13,7 +13,7 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Exercício 1
+// Exercício 1
 
   function criaDatas(dezDaysList){
   for (dia of dezDaysList) {
@@ -30,3 +30,13 @@ function createDaysOfTheWeek() {
 }
 criaDatas([29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]);
 
+// Exercício 2
+
+function addFeriado(Feriados){
+  let buttonFeriado = document.createElement('button');
+  buttonFeriado.id = 'btn-holiday';
+  buttonFeriado.name = Feriados;
+  document.querySelector('.buttons-container').appendChild(buttonFeriado);
+}
+
+addFeriado('Teste');
