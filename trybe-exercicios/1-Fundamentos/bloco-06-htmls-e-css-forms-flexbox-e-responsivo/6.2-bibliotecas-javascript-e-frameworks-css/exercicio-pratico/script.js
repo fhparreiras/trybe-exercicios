b3293d4baf2a9251738f4f3ba0ddc9c49c04988d
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2021-11-28 18:19:33 -0300

    Andamento exercício 6.2

--- trybe-exercicios/1-Fundamentos/bloco-06-htmls-e-css-forms-flexbox-e-responsivo/6.2-bibliotecas-javascript-e-frameworks-css/exercicio-pratico/script.js
+++ trybe-exercicios/1-Fundamentos/bloco-06-htmls-e-css-forms-flexbox-e-responsivo/6.2-bibliotecas-javascript-e-frameworks-css/exercicio-pratico/script.js
@@ -2,21 +2,13 @@ const estadoSelector = document.getElementById('estado');
 let estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 
 'RR', 'SC', 'SP', 'SE', 'TO'];
 
+
 for (let i = 0; i < estados.length; i += 1) {
     let opcaoEstado = document.createElement('option');
     opcaoEstado.innerText = estados[i];
     opcaoEstado.value = estados[i];
     estadoSelector.appendChild(opcaoEstado);
 }
-
-let data = document.getElementById('data-inicio').value;
-if ((data[0] + data[1]) < 1 || (data[0] + data[1]) > 31) {
-    alert("Erro! O dia deve ser maior que 0 e menor ou igual a 31");
-} else if ((data[3] + data[4]) < 1 || (data[3] + data[4]) > 12) {
-    alert("Erro! O mês deve ser maior que 0 e menor ou igual a 12");
-} else if ((data[6] + data[7] + data[8] + data[9]) < 0) { 
-    alert("Erro! O ano não pode ser negativo")
-} 
 
 let nome = document.getElementById('nome');
 let email = document.getElementById('email');
@@ -33,6 +25,8 @@ let descCargo = document.getElementById('desc-cargo');
 let dataInicio = document.getElementById('data-inicio');
 let textoOculto = document.getElementById('oculto');
 
+dataInicio.DatePickerX.init({format: 'dd-mm-yyyy'});
+
 function criaDiv() {
     let novaDiv = document.createElement('div');
     novaDiv.id = 'novaDiv';
@@ -43,7 +37,7 @@ function criaDiv() {
 
 let btnEnviar = document.getElementById('envia-dados');
     btnEnviar.addEventListener('click', (event) => {
-        event.preventDefault();
+        //event.preventDefault();
         if (document.querySelector('#novaDiv') !== null) {
         document.querySelector('#novaDiv').remove();
         }
@@ -58,35 +52,9 @@ let btnEnviar = document.getElementById('envia-dados');
         document.getElementById('resultado-cargo').innerText = 'Cargo: ' + nome.value;
         document.getElementById('resultado-desc-cargo').innerText = 'Descrição do cargo: ' + descCargo.value;
         document.getElementById('resultado-data').innerText = 'Data de início: ' + dataInicio.value;
-        verficaDados();
+        //criaDiv();
 })
 
-function verficaDados() {
-    if (nome.value.length === 0) {
-        alert("Nome inválido.");
-    } else if (email.value.length === 0) {
-        alert("E-mail inválido.");
-    } else if (cpf.value.length === 0) {
-        alert("CPF inválido.");
-    } else if (endereco.value.length === 0) {
-        alert("Endereço inválido.");
-    } else if (cidade.value.length === 0) {
-        alert("Cidade inválida.");
-    } else if (!tipo1.checked && !tipo2.checked) {
-        alert("Tipo de imóvel inválido.");
-    } else if (resumo.value.length === 0) {
-        alert("Resumo inválido.");
-    } else if (cargo.value.length === 0) {
-        alert("Cargo inválido.");
-    } else if (descCargo.value.length === 0) {
-        alert("Descrição do cargo inválida.");
-    } else if (dataInicio.value.length === 0) {
-        alert("Data inválida");
-    } else {
-        alert('Tudo certo com o formulário!')
-        criaDiv();
-    }
-}
 
 let btnLimpar = document.getElementById('limpa-tudo');
     btnLimpar.addEventListener('click', (event) => {
